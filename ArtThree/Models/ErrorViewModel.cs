f7b51a9ab3e53507a54be7728e68ea8955586c16
Arcadyb <arcadyb@gmail.com>
2018-11-01 07:06:45 +0200

    Add project files.

---
+++ ArtThree/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ArtThree.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
